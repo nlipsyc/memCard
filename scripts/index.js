3b646cbe8ac65c1513210b8fba1b897480c6b16f
Noah Lipsyc <noah@Noahs-MacBook-Pro.local>
2016-07-03 15:55:06 -0400

    Will out put a zip file with png of member info.  Currently zip breaks after 100 cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
 console.log("JS loaded");
 console.log("document", document);
-papa = require("papaparse"),
-memJson = [],
+
+
+var papa = require("papaparse"),
+    JSZip = require("jszip"),
+    zip = new JSZip(),
+
+    memJson = [],
+    cards = [];
 
 window.grabCsv = function (){
     csv={};
@@ -14,19 +20,72 @@ window.grabCsv = function (){
 // Convert to JSON
      memJson = papa.parse(file, {
         complete: function(memJson){
-            console.log("memJson", memJson);
-            drawCard(memJson.data[9]);
+        
+        console.log("memJson", memJson);
+        
+        // for(var i = 1; i < memJson.data.length; i++){
+        for(var i = 1; i < 100; i++){
+            drawCard(memJson.data[i]);
         }
-    });
+        console.log('CARDS', cards);
 
-     function drawCard(data){
+        console.log('support blob?', JSZip.support.blob);
+        zipIt(memJson);
+    }
+    });
+};
+
+function drawCard(data){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
         
-        ctx.font = "48px serif";
+        ctx.clearRect(0, 0, canvas.width, canvas.height);  //Clear canvas
+        ctx.font = "32px serif";
         ctx.fillText(data.join(), 50, 75);
 
-    var png = canvas.toDataURL();
-    window.location.href=png;
+        var b64 = canvas.toDataURL('image/png'),
+            blob = dataURItoBlob(b64);
+
+            cards.push(blob);
+
+        
+        console.log('cards');
+
+        function dataURItoBlob(dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0)
+                byteString = atob(dataURI.split(',')[1]);
+            else
+                byteString = unescape(dataURI.split(',')[1]);
+
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {type:mimeString});
+        }
     }
-};
+
+function zipIt(memJson){
+    //for(i = 0; i < cards.length; i++){
+    for(i = 0; i < 100; i++){
+        zip.file('membershipCard'+ memJson.data[i+1][0] + memJson.data[i+1][1] + '.png', cards[i]);
+    }
+    console.log(zip);
+    // zip.generateAsync({type:"blob"}).then(function (blob) {
+    //         saveAs(blob, "memCards.zip");
+    //     }, function(err){
+    //     });
+
+    zip.generateAsync({type:"base64"}).then(function (base64) {
+            window.location = "data:application/zip;base64," + base64;
+    }, function (err) {
+      // shouldn't happen with a base64...
+    });
+}
